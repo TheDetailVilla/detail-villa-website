35626626400d899b3483a4d8f3d6e3f3172ea001
Erick Villa <edv.mrv.2005@gmail.com>
2025-07-04 13:27:14 -0600

    Fix logo in navigation header

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -23,7 +23,7 @@ export default function Navigation() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center space-x-3">
-            <img src="/fullLogo_transparent.png" alt="The Detail Villa Logo" className="h-11 w-11" />
+            <img src="/FullLogo_Transparent.png" alt="The Detail Villa Logo" className="h-11 w-11" />
             <Link href="/" className="text-2xl font-bold text-blue-400">
               The Detail Villa
             </Link>
